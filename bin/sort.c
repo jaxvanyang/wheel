d5091c67f085664f0775af0cf6529a1d1b7f2865
Jax Young <jaxvanyang@gmail.com>
2025-08-11 22:41:34 +0800

    feat(bin): add sort

---
+++ bin/sort.c
@@ -0,0 +1,23 @@
+#include <basics.h>
+
+int main() {
+	Slist *list = slist_new();
+
+	while (true) {
+		Str *s = str_new();
+		str_readline(s, stdin);
+
+		if (s->length == 0) {
+			break;
+		}
+
+		slist_push(list, s);
+	}
+
+	qsort_s(list);
+
+	for (usize i = 0; i < list->length; ++i) {
+		Str *s = slist_get(list, i);
+		printf("%s", s->data);
+	}
+}
