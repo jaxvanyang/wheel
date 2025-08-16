d0af88407f41600ed8d7582ec06125cf4100ee3f
Jax Young <jaxvanyang@gmail.com>
2025-08-16 21:58:47 +0800

    feat(bin): add hex2double & hex2float

---
+++ bin/hex2double.c
@@ -0,0 +1,24 @@
+//! Inspect IEEE 754 float number.
+//!
+//! Usage: hex2double <64-bit hex ...>
+
+#include <basics.h>
+
+int main(int argc, char **argv) {
+	if (argc <= 1) {
+		return 1;
+	}
+
+	for (usize i = 1; i < argc; ++i) {
+		char *hex = argv[i];
+		f64 number;
+
+		sscanf(hex, "%llX", (u64 *)&number);
+
+		u64 *hex_number = (u64 *)&number;
+		Float f = decode_f64(number);
+
+		printf(
+			"%llX: %hhd x %lf x 2^%lld = %f\n", *hex_number, f.sign, f.m, f.exponent, number);
+	}
+}
---
+++ bin/hex2float.c
@@ -0,0 +1,24 @@
+//! Inspect IEEE 754 float number.
+//!
+//! Usage: hex2float <32-bit hex ...>
+
+#include <basics.h>
+
+int main(int argc, char **argv) {
+	if (argc <= 1) {
+		return 1;
+	}
+
+	for (usize i = 1; i < argc; ++i) {
+		char *hex = argv[i];
+		f32 number;
+
+		sscanf(hex, "%X", (u32 *)&number);
+
+		u32 *hex_number = (u32 *)&number;
+		Float f = decode_f32(number);
+
+		printf(
+			"%X: %hhd x %lf x 2^%lld = %f\n", *hex_number, f.sign, f.m, f.exponent, number);
+	}
+}
