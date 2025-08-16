0cba50a1f3f636311c802cbcb77548bb32a70bd6
Jax Young <jaxvanyang@gmail.com>
2025-08-16 21:57:03 +0800

    feat(core): add more integer types

--- src/basics/core/types.h
+++ src/basics/core/types.h
@@ -3,13 +3,22 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
-typedef u_int64_t u64;
+typedef u_int8_t u8;
+typedef int8_t i8;
+
+typedef u_int16_t u16;
+typedef int16_t i16;
+
 typedef u_int32_t u32;
-typedef int64_t i64;
 typedef int32_t i32;
-typedef float f32;
-typedef double f64;
+
+typedef u_int64_t u64;
+typedef int64_t i64;
+
 typedef size_t usize;
 typedef long isize;
+
+typedef float f32;
+typedef double f64;
 
 extern const usize USIZE_MAX;
