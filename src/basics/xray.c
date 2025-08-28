a8a85ca0bdf650a4acb9c2a5494b83e4aa71ded4
Jax Young <jaxvanyang@gmail.com>
2025-08-28 22:30:59 +0800

    feat(xray): new module to extend raylib

---
+++ src/basics/xray.c
@@ -0,0 +1,6 @@
+#include "xray.h"
+#include <raymath.h>
+
+void draw_entity(Entity entity) {
+	DrawTexturePro(entity.texture, entity.source, entity.dest, Vector2Zero(), 0, WHITE);
+}
---
+++ src/basics/xray.h
@@ -0,0 +1,14 @@
+// Extension to raylib
+#pragma once
+
+#include "core.h"
+#include <raylib.h>
+
+typedef struct {
+	Texture2D texture;
+	Rectangle source;
+	Rectangle dest;
+	Rectangle hitbox;
+} Entity;
+
+void draw_entity(Entity entity);
