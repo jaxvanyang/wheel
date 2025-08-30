4213bc79ccad9f371c16202f28877e198f5b3603
Jax Young <jaxvanyang@gmail.com>
2025-08-30 10:25:43 +0800

    feat(xray): add EntityList

--- src/basics/xray.c
+++ src/basics/xray.c
@@ -4,3 +4,50 @@
 void draw_entity(Entity entity) {
 	DrawTexturePro(entity.texture, entity.source, entity.dest, Vector2Zero(), 0, WHITE);
 }
+
+EntityList *elist_new() {
+	EntityList *list = malloc(sizeof(EntityList));
+	list->size = 0;
+	list->head = NULL;
+
+	return list;
+}
+
+void elist_free(EntityList *list) {
+	EntityNode *node = list->head;
+
+	while (node) {
+		EntityNode *next = node->next;
+		node->next = NULL;
+		free(node);
+		node = next;
+	}
+
+	list->head = NULL;
+	list->size = 0;
+}
+
+void elist_insert(EntityList *list, Entity entity) {
+	EntityNode *node = malloc(sizeof(EntityNode));
+	node->value = entity;
+
+	node->next = list->head;
+	list->head = node;
+	++list->size;
+}
+
+Entity elist_pop(EntityList *list) {
+	if (list->size == 0) {
+		error("expected non-zero size list");
+	}
+
+	EntityNode *node = list->head;
+	Entity entity = node->value;
+
+	list->head = node->next;
+	--list->size;
+	node->next = NULL;
+	free(node);
+
+	return entity;
+}
--- src/basics/xray.h
+++ src/basics/xray.h
@@ -11,4 +11,19 @@ typedef struct {
 	Rectangle hitbox;
 } Entity;
 
+typedef struct EntityNode {
+	Entity value;
+	struct EntityNode *next;
+} EntityNode;
+
+typedef struct {
+	usize size;
+	EntityNode *head;
+} EntityList;
+
 void draw_entity(Entity entity);
+
+EntityList *elist_new();
+void elist_free(EntityList *list);
+void elist_insert(EntityList *list, Entity entity);
+Entity elist_pop(EntityList *list);
