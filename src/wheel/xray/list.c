2b7761f8c01b1bb953fcb56758f41d0f5934d372
Jax Young <jaxvanyang@gmail.com>
2025-09-12 10:13:30 +0800

    refactor(xray): use macro to implement list functions

--- src/wheel/xray/list.c
+++ src/wheel/xray/list.c
@@ -1,103 +1,107 @@
 #include "list.h"
 
-EntityList *elist_new() {
-	EntityList *list = malloc(sizeof(EntityList));
-	list->size = 0;
-	list->head = list->tail = NULL;
-
-	return list;
-}
-
-void elist_free(EntityList *list) {
-	EntityNode *node = list->head;
-
-	while (node) {
-		EntityNode *next = node->next;
-		node->next = NULL;
-		free(node);
-		node = next;
+#define DEQUEUE_IMPLEMENTATION(T, NodeName, Name, prefix) \
+\
+	Name *prefix##_new() { \
+		Name *list = malloc(sizeof(Name)); \
+		list->size = 0; \
+		list->head = list->tail = NULL; \
+\
+		return list; \
+	} \
+\
+	void prefix##_free(Name *list) { \
+		NodeName *node = list->head; \
+\
+		while (node) { \
+			NodeName *next = node->next; \
+			node->next = NULL; \
+			free(node); \
+			node = next; \
+		} \
+\
+		list->head = list->tail = NULL; \
+		list->size = 0; \
+	} \
+\
+	void prefix##_push_front(Name *list, T value) { \
+		NodeName *node = malloc(sizeof(NodeName)); \
+		node->value = value; \
+\
+		node->prev = NULL; \
+		node->next = list->head; \
+\
+		if (list->size) { \
+			list->head->prev = node; \
+			list->head = node; \
+		} else { \
+			list->head = list->tail = node; \
+		} \
+\
+		++list->size; \
+	} \
+\
+	void prefix##_push_back(Name *list, T value) { \
+		NodeName *node = malloc(sizeof(NodeName)); \
+		node->value = value; \
+\
+		node->prev = list->tail; \
+		node->next = NULL; \
+\
+		if (list->size) { \
+			list->tail->next = node; \
+			list->tail = node; \
+		} else { \
+			list->head = list->tail = node; \
+		} \
+\
+		++list->size; \
+	} \
+\
+	T prefix##_pop_front(Name *list) { \
+		if (list->size == 0) { \
+			error("expected non-empty list"); \
+		} \
+\
+		NodeName *node = list->head; \
+		T value = node->value; \
+\
+		list->head = node->next; \
+		--list->size; \
+		node->prev = node->next = NULL; \
+		free(node); \
+\
+		if (list->size == 0) { \
+			list->tail = NULL; \
+		} \
+\
+		return value; \
+	} \
+\
+	T prefix##_pop_back(Name *list) { \
+		if (list->size == 0) { \
+			error("expected non-empty list"); \
+		} \
+\
+		NodeName *node = list->tail; \
+		T value = node->value; \
+\
+		list->tail = node->prev; \
+		--list->size; \
+		node->prev = node->next = NULL; \
+		free(node); \
+\
+		if (list->size == 0) { \
+			list->head = NULL; \
+		} \
+\
+		return value; \
+	} \
+\
+	void prefix##_clear(Name *list) { \
+		while (list->size != 0) { \
+			prefix##_pop_front(list); \
+		} \
 	}
 
-	list->head = list->tail = NULL;
-	list->size = 0;
-}
-
-void elist_push_front(EntityList *list, Entity entity) {
-	EntityNode *node = malloc(sizeof(EntityNode));
-	node->value = entity;
-
-	node->prev = NULL;
-	node->next = list->head;
-
-	if (list->size) {
-		list->head->prev = node;
-		list->head = node;
-	} else {
-		list->head = list->tail = node;
-	}
-
-	++list->size;
-}
-
-void elist_push_back(EntityList *list, Entity entity) {
-	EntityNode *node = malloc(sizeof(EntityNode));
-	node->value = entity;
-
-	node->prev = list->tail;
-	node->next = NULL;
-
-	if (list->size) {
-		list->tail->next = node;
-		list->tail = node;
-	} else {
-		list->head = list->tail = node;
-	}
-
-	++list->size;
-}
-
-Entity elist_pop_front(EntityList *list) {
-	if (list->size == 0) {
-		error("expected non-empty list");
-	}
-
-	EntityNode *node = list->head;
-	Entity entity = node->value;
-
-	list->head = node->next;
-	--list->size;
-	node->prev = node->next = NULL;
-	free(node);
-
-	if (list->size == 0) {
-		list->tail = NULL;
-	}
-
-	return entity;
-}
-
-Entity elist_pop_back(EntityList *list) {
-	if (list->size == 0) {
-		error("expected non-empty list");
-	}
-
-	EntityNode *node = list->tail;
-	Entity entity = node->value;
-
-	list->tail = node->prev;
-	--list->size;
-	node->prev = node->next = NULL;
-	free(node);
-
-	if (list->size == 0) {
-		list->head = NULL;
-	}
-
-	return entity;
-}
-
-void elist_clear(EntityList *list) {
-	while (list->size != 0) {
-		elist_pop_front(list);
-	}
-}
+DEQUEUE_IMPLEMENTATION(Entity, EntityNode, EntityList, elist)
--- src/wheel/xray/list.h
+++ src/wheel/xray/list.h
@@ -6,8 +6,8 @@ DEQUEUE(Entity, EntityNode, EntityList)
 
 EntityList *elist_new();
 void elist_free(EntityList *list);
-void elist_push_front(EntityList *list, Entity entity);
-void elist_push_back(EntityList *list, Entity entity);
+void elist_push_front(EntityList *list, Entity value);
+void elist_push_back(EntityList *list, Entity value);
 Entity elist_pop_front(EntityList *list);
 Entity elist_pop_back(EntityList *list);
 void elist_clear(EntityList *list);
