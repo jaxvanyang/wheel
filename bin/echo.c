0bb623dd327503051f400298d8168cfaac9cf22a
Jax Young <jaxvanyang@gmail.com>
2025-09-21 14:10:33 +0800

    feat(bin): echo: listen both on UDP and TCP ports

--- bin/echo.c
+++ bin/echo.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <errno.h>
+#include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 #include <wheel.h>
@@ -10,18 +11,8 @@ typedef struct {
 	int tcp_sock;
 } Server;
 
-int main(int argc, const char **argv) {
-	if (argc != 2) {
-		error("expected one and only one argument");
-	}
-
-	u16 port = 0;
-	sscanf(argv[1], "%hu", &port);
-
-	char buffer[1024];
-	char resp[1024];
-
-	Server server = {.sa = {.addr = INADDR_ANY, .port = port}};
+Server new_server(u32 ip, u16 port) {
+	Server server = {.sa = {.ip = ip, .port = port}};
 	server.udp_sock = new_udp_socket();
 	server.tcp_sock = new_tcp_socket();
 
@@ -41,9 +32,13 @@ int main(int argc, const char **argv) {
 		error("failed to listen for TCP connenction");
 	}
 
-	char *addr = format_sa(server.sa);
-	printf("server: listening on %s...\n", addr);
-	FREE(addr);
+	return server;
+}
+
+void *handle_udp(void *arg) {
+	Server server = *(Server *)arg;
+	char buffer[1024];
+	char resp[1024];
 
 	while (true) {
 		SockAddr client;
@@ -60,9 +55,78 @@ int main(int argc, const char **argv) {
 
 		snprintf(resp, sizeof(resp), "Echo: %.*s", (int)len, buffer);
 		send_to(server.udp_sock, client, resp, strlen(resp), 0);
-
 	}
 
+	return NULL;
+}
+
+void *handle_tcp(void *arg) {
+	Server server = *(Server *)arg;
+	char buffer[1024];
+	char resp[1024];
+
+	while (true) {
+		SockAddr client;
+		int conn_sock = net_accept(server.tcp_sock, &client);
+		if (conn_sock == -1) {
+			error("failed to accept TCP connection");
+		}
+		char *addr = format_sa(client);
+
+		while (true) {
+			isize len = net_recv(conn_sock, (void *)buffer, sizeof(buffer), 0);
+
+			if (len == -1) {
+				perror("failed to receive from TCP connection");
+
+				if (shutdown(conn_sock, SHUT_RDWR) == -1) {
+					perror("failed to shutdown TCP connection");
+				}
+
+				exit(EXIT_FAILURE);
+			} else if (len == 0) {
+				assert(shutdown(conn_sock, SHUT_WR) == 0);
+				break;
+			}
+
+			printf("received %ld bytes from TCP %s:\n", len, addr);
+			printf("> %.*s\n", (int)len, buffer);
+
+			snprintf(resp, sizeof(resp), "Echo: %.*s", (int)len, buffer);
+			if (send(conn_sock, resp, strlen(resp), 0) < 0) {
+				perror("failed to send to TCP connection");
+			}
+		}
+
+		FREE(addr);
+
+		CLOSE(conn_sock);
+	}
+
+	return NULL;
+}
+
+int main(int argc, const char **argv) {
+	if (argc != 2) {
+		error("expected one and only one argument");
+	}
+
+	u16 port = 0;
+	sscanf(argv[1], "%hu", &port);
+
+	Server server = new_server(INADDR_ANY, port);
+
+	char *addr = format_sa(server.sa);
+	printf("server: listening on %s...\n", addr);
+	FREE(addr);
+
+	pthread_t udp_thread;
+	pthread_create(&udp_thread, NULL, handle_udp, &server);
+	pthread_t tcp_thread;
+	pthread_create(&tcp_thread, NULL, handle_tcp, &server);
+
+	pthread_join(udp_thread, NULL);
 	CLOSE(server.udp_sock);
+	pthread_join(tcp_thread, NULL);
 	CLOSE(server.tcp_sock);
 }
