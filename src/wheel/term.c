e8fcd891991fcc1ec5aa41f24741c7150368c7f1
Jax Young <jaxvanyang@gmail.com>
2025-09-23 17:21:25 +0800

    feat(term): add term_clear_line()

--- src/wheel/term.c
+++ src/wheel/term.c
@@ -3,3 +3,5 @@
 #include "term.h"
 
 void term_clear() { printf("\e[2J\e[1;1H"); }
+
+void term_clear_line() { printf("\e[F\e[2K"); }
--- src/wheel/term.h
+++ src/wheel/term.h
@@ -3,3 +3,4 @@
 // For more information: https://en.wikipedia.org/wiki/ANSI_escape_code
 
 void term_clear();
+void term_clear_line();
