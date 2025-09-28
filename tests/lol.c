e28e6a52e803ecedcb15ef9897419c937226c325
Jax Young <jaxvanyang@gmail.com>
2025-09-28 22:33:22 +0800

    style: format code

--- tests/lol.c
+++ tests/lol.c
@@ -5,97 +5,95 @@
 
 #include "wheel/lol.h"
 
-#define THREAD_CNT     1000
+#define THREAD_CNT 1000
 #define THREAD_LOG_CNT 1000
 
-static void *log_thread(void *arg)
-{
-    pthread_t tid = pthread_self();
-    char tid_str[32];
-    snprintf(tid_str, sizeof(tid_str), "%lu", (unsigned long)tid);
+static void *log_thread(void *arg) {
+	pthread_t tid = pthread_self();
+	char tid_str[32];
+	snprintf(tid_str, sizeof(tid_str), "%lu", (unsigned long)tid);
 
-    // FIXME: pthread_t is pointer on macOS, so difference between tids may not
-    // be 1
-    lol_init(tid_str, (unsigned long)tid % 6 + 1, NULL, LOL_NONE);
+	// FIXME: pthread_t is pointer on macOS, so difference between tids may not
+	// be 1
+	lol_init(tid_str, (unsigned long)tid % 6 + 1, NULL, LOL_NONE);
 
-    for (int i = 0; i < *(int *)arg; i++) {
-        lol_fatal2(tid_str, "");
-        lol_error2(tid_str, "");
-        lol_warn2(tid_str, "");
-        lol_info2(tid_str, "");
-        lol_debug2(tid_str, "");
-        lol_trace2(tid_str, "");
-    }
+	for (int i = 0; i < *(int *)arg; i++) {
+		lol_fatal2(tid_str, "");
+		lol_error2(tid_str, "");
+		lol_warn2(tid_str, "");
+		lol_info2(tid_str, "");
+		lol_debug2(tid_str, "");
+		lol_trace2(tid_str, "");
+	}
 
-    return arg;
+	return arg;
 }
 
-static void test_errno()
-{
-    lol_init("errno", LOL_INFO, NULL, LOL_NONE);
-    FILE *f = fopen("nonexist", "r");
-    // perror("");
-    lol_error_e("");
-    lol_error_e2(errno, "");
-    lol_error_de("errno", errno, "");
-    if (f) {
-        fclose(f);
-    }
+static void test_errno() {
+	lol_init("errno", LOL_INFO, NULL, LOL_NONE);
+	FILE *f = fopen("nonexist", "r");
+	// perror("");
+	lol_error_e("");
+	lol_error_e2(errno, "");
+	lol_error_de("errno", errno, "");
+	if (f) {
+		fclose(f);
+	}
 }
 
-static void print_usage(FILE *out, const char *program)
-{
-    fprintf(out, "Usage: %s [-t <thread-count>] [-l <log-count>] [-h]\n",
-            program);
+static void print_usage(FILE *out, const char *program) {
+	fprintf(out, "Usage: %s [-t <thread-count>] [-l <log-count>] [-h]\n", program);
 }
 
-int main(int argc, char **argv)
-{
-    int opt;
-    int thr_cnt = 0;
-    int log_cnt = 0;
-    while ((opt = getopt(argc, argv, "t:l:h")) != -1) {
-        switch (opt) {
-        case 't':
-            thr_cnt = atoi(optarg);
-            break;
-        case 'l':
-            log_cnt = atoi(optarg);
-            break;
-        case 'h':
-            print_usage(stdout, argv[0]);
-            exit(EXIT_SUCCESS);
-        default: /* '?' */
-            print_usage(stderr, argv[0]);
-            exit(EXIT_FAILURE);
-        }
-    }
+int main(int argc, char **argv) {
+	int opt;
+	int thr_cnt = 0;
+	int log_cnt = 0;
+	while ((opt = getopt(argc, argv, "t:l:h")) != -1) {
+		switch (opt) {
+		case 't':
+			thr_cnt = atoi(optarg);
+			break;
+		case 'l':
+			log_cnt = atoi(optarg);
+			break;
+		case 'h':
+			print_usage(stdout, argv[0]);
+			exit(EXIT_SUCCESS);
+		default: /* '?' */
+			print_usage(stderr, argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
 
-    if (thr_cnt > THREAD_CNT || log_cnt > THREAD_LOG_CNT) {
-        fprintf(stderr,
-                "The exec count must be less than %d and The log count must be "
-                "less than %d\n",
-                THREAD_CNT, THREAD_LOG_CNT);
-        exit(EXIT_FAILURE);
-    }
-    if (thr_cnt <= 0) {
-        thr_cnt = 4;
-    }
-    if (log_cnt <= 0) {
-        log_cnt = 4;
-    }
+	if (thr_cnt > THREAD_CNT || log_cnt > THREAD_LOG_CNT) {
+		fprintf(
+			stderr,
+			"The exec count must be less than %d and The log count must be "
+			"less than %d\n",
+			THREAD_CNT,
+			THREAD_LOG_CNT
+		);
+		exit(EXIT_FAILURE);
+	}
+	if (thr_cnt <= 0) {
+		thr_cnt = 4;
+	}
+	if (log_cnt <= 0) {
+		log_cnt = 4;
+	}
 
-    pthread_t threads[THREAD_CNT];
-    for (int i = 0; i < thr_cnt; i++) {
-        pthread_create(threads + i, NULL, log_thread, &log_cnt);
-    }
+	pthread_t threads[THREAD_CNT];
+	for (int i = 0; i < thr_cnt; i++) {
+		pthread_create(threads + i, NULL, log_thread, &log_cnt);
+	}
 
-    for (int i = 0; i < thr_cnt; i++) {
-        pthread_join(threads[i], NULL);
-    }
+	for (int i = 0; i < thr_cnt; i++) {
+		pthread_join(threads[i], NULL);
+	}
 
-    test_errno();
+	test_errno();
 
-    lol_fini();
-    return 0;
+	lol_fini();
+	return 0;
 }
