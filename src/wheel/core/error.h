22d2e7f44f3aef05ff9476c83b1e7a9f258eb961
Jax Young <jaxvanyang@gmail.com>
2025-10-10 14:05:14 +0800

    feat(core): add marco todo()

--- src/wheel/core/error.h
+++ src/wheel/core/error.h
@@ -7,4 +7,10 @@
 	fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
 	fprintf(stderr, __VA_ARGS__); \
 	fprintf(stderr, "\n"); \
-	exit(1)
+	exit(EXIT_FAILURE);
+
+#define todo(...) \
+	fprintf(stderr, "%s:%d: TODO: ", __FILE__, __LINE__); \
+	fprintf(stderr, __VA_ARGS__); \
+	fprintf(stderr, "\n"); \
+	exit(EXIT_FAILURE);
